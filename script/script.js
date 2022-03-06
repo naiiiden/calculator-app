f5a85f23dc2e4937520fb7176cd053ae48fa018e
naiden <na4ko359@gmail.com>
2022-03-06 22:39:37 +0200

    add regex preventing from typing characters in the input field

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 let num1, num2, operator;
 let input = document.querySelector("#input");
+input.addEventListener("keypress", e => {
+        if (!/[0-9\./]+/.test(e.key)) { //PREVENTS FROM TYPING CHARACTERS WHICH ARE DIFFERENT THAN 0-9 AND .
+        e.preventDefault();
+    };
+});
 
 document.querySelectorAll(".number").forEach(number => {
     number.addEventListener("click", () => {
