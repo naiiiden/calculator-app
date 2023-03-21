430702e70516fca477177bf4f71a3a5f3e6998d9
naiiiden <na4ko359@gmail.com>
2023-03-21 06:21:31 +0200

    keyboard pressing improvements

--- script/script.js
+++ script/script.js
@@ -80,9 +80,9 @@ if (localStorage.getItem("range", range.value) == 1) {
 }
 
 document.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-        case 8: input.textContent = input.textContent.substring(0, input.textContent.length - 1);
-        case 13: num2 = parseFloat(input.textContent.substring(input.textContent.indexOf(operator) + 1));
+    switch (e.key) {
+        case "Backspace": input.textContent = input.textContent.substring(0, input.textContent.length - 1);
+        case "Enter": num2 = parseFloat(input.textContent.substring(input.textContent.indexOf(operator) + 1));
         switch (operator) {
             case "+": input.textContent = num1 + num2; num1 += num2; break;
             case "-": input.textContent = num1 - num2; num1 -= num2; break;
@@ -91,25 +91,26 @@ document.addEventListener("keydown", (e) => {
             case "^": input.textContent = num1 ** num2; num1 **= num2; break;
         }; break;
         /*OPERATORS*/
-        case 42: operator = "x"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
-        case 43: operator = "+"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
-        case 45: operator = "-"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
-        case 47: operator = "/"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        case "*": operator = "x"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        case "+": operator = "+"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        case "-": operator = "-"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        case "/": operator = "/"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        case "^": operator = "^"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
         /*NUMBERS*/
-        case 48: input.textContent += 0; break;
-        case 49: input.textContent += 1; break;
-        case 50: input.textContent += 2; break;
-        case 51: input.textContent += 3; break;
-        case 52: input.textContent += 4; break;
-        case 53: input.textContent += 5; break;
-        case 54: input.textContent += 6; break;
-        case 55: input.textContent += 7; break;
-        case 56: input.textContent += 8; break;
-        case 57: input.textContent += 9; break;
+        case "0": input.textContent += 0; break;
+        case "1": input.textContent += 1; break;
+        case "2": input.textContent += 2; break;
+        case "3": input.textContent += 3; break;
+        case "4": input.textContent += 4; break;
+        case "5": input.textContent += 5; break;
+        case "6": input.textContent += 6; break;
+        case "7": input.textContent += 7; break;
+        case "8": input.textContent += 8; break;
+        case "9": input.textContent += 9; break;
         /*EQUALS*/
         /*RESET AND DEL*/
         // EXPONENTIATION OPERATOR IS HERE SO I DON'T BREAK THE CASE ORDER
-        case 94: operator = "^"; if (num1 === undefined) num1 = parseFloat(input.textContent); input.textContent += operator; break;
+        // ?????? i'll figure out what these two cases are
         case 99: num1 = undefined; num2 = undefined; operator = undefined; input.textContent = ""; break;
         case 127: input.textContent = ""; break;
     }
